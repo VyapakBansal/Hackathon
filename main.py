c26877643b0555212b00c58338eb2bf3b8b26ffd
Riley Willard <rileymwillard@gmail.com>
2025-11-14 13:23:59 -0700

    trying this again

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import numpy as np
 import pandas as pd
-import analysis as question5
 
-question5.analysis() # this is rileys method for question 5
+data_frame = pd.read_csv('HackathonDataset.csv')
